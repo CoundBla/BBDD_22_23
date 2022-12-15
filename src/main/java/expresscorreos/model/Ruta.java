eb8b1271406b83f5a1f9b60e0cc2a4d769f0c841
Alvaro <a.fyanez@alumnos.upm.es>
2022-12-15 19:50:00 +0100

    Paquete recogida y reparto y ruta hechos

--- src/main/java/expresscorreos/model/Ruta.java
+++ src/main/java/expresscorreos/model/Ruta.java
@@ -1,4 +1,17 @@
 package expresscorreos.model;
 
 public class Ruta {
+    private int id_ruta;
+
+    public Ruta(int id_ruta) {
+        this.id_ruta = id_ruta;
+    }
+
+    public int getId_ruta() {
+        return id_ruta;
+    }
+
+    public void setId_ruta(int id_ruta) {
+        this.id_ruta = id_ruta;
+    }
 }
